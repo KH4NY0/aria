da4e5a4b6f6d0312bdc850db1a95a08a11f1905e
Lukhanyo Radebe <khanyoradebe46@gmail.com>
2024-11-21 02:36:13 +0200

    Create test user

--- actions/signup.ts
+++ actions/signup.ts
@@ -1,15 +1,36 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 import { SignUpSchema } from "@/schemas";
+import {getUserByEmail} from "@/data/user";
 
 export const signup = async (values: z.infer<typeof SignUpSchema>) => {
     const validatedFields = SignUpSchema.safeParse(values);
 
-
     if (!validatedFields.success) {
         return {error: "Invalid fields!"};
     }
+
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: "Email already exists" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    // TODO: Send verification token email
 
     return { success: "Account Created Successfully" };
 
